310b01d7121942eb504353b06d6eadf47dd07d25
The Cooler Kirin <52986010+dipshit@users.noreply.github.com>
2019-08-22 01:45:21 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ if [ ! -f BuildTools.jar ]; then
 	curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 fi
 
-java -Xmx1024M -jar BuildTools.jar --rev "$@"
+java -Xmx3G -jar BuildTools.jar --rev "$@"
