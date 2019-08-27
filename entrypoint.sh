1d9f3d9f66eeed9d291245302a42f6861eb52db3
Kirin Rastogi <dipshit.dev@gmail.com>
2019-08-27 10:32:00 -0400

    overwrite config files with symlink

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # symlink config files from the volume at /home/minecraft/config to /home/minecraft/server
-ln -s /home/minecraft/config/ops.json /home/minecraft/server/ops.json
-ln -s /home/minecraft/config/whitelist.json /home/minecraft/server/whitelist.json
-ln -s /home/minecraft/config/server.properties /home/minecraft/server/server.properties
-ln -s /home/minecraft/config/spigot.yml /home/minecraft/server/spigot.yml
+ln -sf /home/minecraft/config/ops.json /home/minecraft/server/ops.json
+ln -sf /home/minecraft/config/whitelist.json /home/minecraft/server/whitelist.json
+ln -sf /home/minecraft/config/server.properties /home/minecraft/server/server.properties
+ln -sf /home/minecraft/config/spigot.yml /home/minecraft/server/spigot.yml
 
 /sbin/tini -g -- java -Xmx4608m -Xms4608m -XX:-UseContainerSupport -jar /jars/spigot.jar nogui
