a7c4150f3db830cd6be72ffabffedaa5a978de0c
Kirin Rastogi <dipshit.dev@gmail.com>
2019-08-27 22:36:46 -0400

    beef up server

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,4 +3,4 @@
 # symlink config files from the volume at /home/minecraft/config to /home/minecraft/server
 ln -sf /home/minecraft/config/whitelist.json /home/minecraft/server/whitelist.json
 
-java -Xmx4608m -Xms4608m -XX:-UseContainerSupport -jar /jars/spigot.jar nogui
+java -Xmx9G -Xms9G -XX:-UseContainerSupport -jar /jars/spigot.jar nogui
