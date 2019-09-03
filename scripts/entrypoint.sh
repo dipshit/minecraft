6cc8ed8e686eb9c572c034a31835494cfd8433ec
Kirin Rastogi <dipshit.dev@gmail.com>
2019-09-03 14:26:08 -0400

    use exec to fwd signals to jvm

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,8 +3,7 @@
 # exit if ram params not set
 set -u
 
-
 # symlink config files from the volume at /home/minecraft/config to /home/minecraft/server
 ln -sf /home/minecraft/config/whitelist.json /home/minecraft/server/whitelist.json
 
-java -Xmx${HEAPSIZE} -Xms${HEAPSIZE} -XX:-UseContainerSupport -jar /jars/spigot.jar nogui
+exec java -Xmx${HEAPSIZE} -Xms${HEAPSIZE} -XX:-UseContainerSupport -jar /jars/spigot.jar nogui
